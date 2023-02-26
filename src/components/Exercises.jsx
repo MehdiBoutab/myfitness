4cbf0eaba041865916e04d9aad55abc2a4bc9bbb
Mehdi Boutab <mehdiboutab@gmail.com>
2023-02-26 17:50:58 +0100

    Modified Exercises HeroBanner components

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 
 const Exercises = () => {
-  return (
-    <div>Exercises</div>
-  )
-}
+  return <div>Exercises</div>;
+};
 
-export default Exercises
+export default Exercises;
