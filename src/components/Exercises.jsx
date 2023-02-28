6c1de78a3d9b10e3a866dce364daa362dd73dbd5
Mehdi Boutab <mehdiboutab@gmail.com>
2023-02-28 16:18:22 +0100

    Adding SearchEcercise

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 
 const Exercises = () => {
   return <div>Exercises</div>;
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('Dos');
+  const handleSearch = () => {
+    if(search){
+      // const exercisesData = await fetchData()
+    };
+  };
   return (
-    <div>SearchExercises</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="49px"
+        textAlign="center"
+      >
+        Awesome Exercices You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "1170px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          placeholder="Search Exercises"
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+          value={search}
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "61px",
+            position: "absolute",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
